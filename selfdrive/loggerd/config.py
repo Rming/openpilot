b99f5e695b454d41f29d11b2d236ea2e602a04d3
Rming <rmingwang@gmail.com>
2020-01-04 00:45:09 +0800

    自动删除 3 天前的驾驶记录

--- selfdrive/loggerd/config.py
+++ selfdrive/loggerd/config.py
@@ -1,4 +1,6 @@
 import os
+import time
+import datetime
 
 if os.environ.get('LOGGERD_ROOT', False):
   ROOT = os.environ['LOGGERD_ROOT']
@@ -17,3 +19,17 @@ def get_available_percent():
       available_percent = 100.0
 
     return available_percent
+
+def get_dirs_xdays_ago(root, xdays=3):
+    today_list = time.strftime("%Y-%m-%d", time.localtime(time.time())).split("-")
+    today = datetime.datetime(int(today_list[0]), int(today_list[1]), int(today_list[2]))
+    dirs = os.listdir(root)
+    for f in dirs:
+        path = os.path.join(root, f)
+        ctime = time.strftime("%Y-%m-%d", time.localtime((os.stat(path)).st_ctime))
+        ctlist = ctime.split("-")
+        ct_day = datetime.datetime(int(ctlist[0]), int(ctlist[1]), int(ctlist[2]))
+        days_diff = (today - ct_day).days
+        if days_diff > xdays:
+            yield path
+
--- selfdrive/loggerd/deleter.py
+++ selfdrive/loggerd/deleter.py
@@ -3,14 +3,18 @@ import os
 import shutil
 import threading
 from selfdrive.swaglog import cloudlog
-from selfdrive.loggerd.config import ROOT, get_available_percent
+from selfdrive.loggerd.config import ROOT, get_available_percent, get_dirs_xdays_ago
 from selfdrive.loggerd.uploader import listdir_by_creation
 
 
 def deleter_thread(exit_event):
   while not exit_event.is_set():
-    available_percent = get_available_percent()
 
+    for delete_path in get_dirs_xdays_ago(ROOT, xdays=3):
+        cloudlog.info("deleting %s" % delete_path)
+        shutil.rmtree(delete_path, ignore_errors=True)
+
+    available_percent = get_available_percent()
     if available_percent < 10.0:
       # remove the earliest directory we can
       dirs = listdir_by_creation(ROOT)
