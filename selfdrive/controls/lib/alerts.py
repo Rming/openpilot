2e1ebc03b4a037f7aa8ff6db02c09bf85620c462
Rming <rmingwang@gmail.com>
2020-01-07 22:16:05 +0800

    转向超出扭矩限制警告移除提示音，只在原车仪表盘提示

--- selfdrive/controls/lib/alerts.py
+++ selfdrive/controls/lib/alerts.py
@@ -93,8 +93,8 @@ ALERTS = [
       "steerSaturated",
       "请求接管",
       "超出方向盘转向限制",
-      AlertStatus.userPrompt, AlertSize.mid,
-      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.chimePrompt, 1., 2., 3.),
+      AlertStatus.userPrompt, AlertSize.none,
+      Priority.LOW, VisualAlert.steerRequired, AudibleAlert.none, 1., 2., 3.),
 
   Alert(
       "steerTempUnavailable",
